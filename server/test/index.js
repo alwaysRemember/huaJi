b070b1acb1e6de05c11e76caf18824bdcf4a72cf
“always” <740905172@qq.com>
2020-12-16 15:55:07 +0800

    update => 修改文件结构,兼容微信云开发

---
+++ server/test/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Always
+ * @LastEditors: Always
+ * @Date: 2020-12-15 19:57:27
+ * @LastEditTime: 2020-12-16 15:49:42
+ * @FilePath: /huaJi/server/test/index.js
+ */
+const cloud = require('wx-server-sdk');
+
+cloud.init();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext();
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  };
+};

c96db0ceceb175f2ad2f4eab48b6a7e9ae6e5cef
“always” <740905172@qq.com>
2021-01-08 11:07:36 +0800

    fix => 修复页面在ios中弹性滑动问题导致tabbar会跟随手动弹性方向滚动

--- client/src/pages/Home/index.config.ts
+++ client/src/pages/Home/index.config.ts
@@ -2,9 +2,10 @@
  * @Author: Always
  * @LastEditors: Always
  * @Date: 2020-12-15 17:21:22
- * @LastEditTime: 2020-12-16 17:59:07
+ * @LastEditTime: 2021-01-08 11:06:08
  * @FilePath: /huaJi/client/src/pages/Home/index.config.ts
  */
 export default {
   navigationBarTitleText: '花记账本',
+  disableScroll: true,
 };
--- client/src/pages/Summary/index.config.ts
+++ client/src/pages/Summary/index.config.ts
@@ -2,11 +2,12 @@
  * @Author: Always
  * @LastEditors: Always
  * @Date: 2020-12-16 18:00:02
- * @LastEditTime: 2021-01-04 15:09:07
+ * @LastEditTime: 2021-01-08 11:04:21
  * @FilePath: /huaJi/client/src/pages/Summary/index.config.ts
  */
 export default {
   navigationBarTitleText: '汇总',
   navigationBarTextStyle: 'white',
   navigationBarBackgroundColor: '#6ac5d7',
+  disableScroll: true,
 };
